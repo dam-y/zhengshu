69c9ba227701f07305f19649e87ea7a034e9f21e
sulei <2750882656@qq.com>
2019-09-24 17:11:58 +0800

    还原hello接口

--- src/main/java/com/cxria/ceshi/api/helloController.java
+++ src/main/java/com/cxria/ceshi/api/helloController.java
@@ -1,8 +1,12 @@
 package com.cxria.ceshi.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class helloController {
-
+    @GetMapping("hello")
+    public String hello(){
+        return "hello,boy";
+    }
 }
